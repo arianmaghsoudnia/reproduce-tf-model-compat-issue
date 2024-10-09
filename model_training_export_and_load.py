be6ab7eb63eaa11a20f99761d7c9351e48593508
Arian Maghsoudnia <arian.maghsoudnia@blockbax.com>
2024-10-09 15:45:00 +0200

    Fix bad call for TFSM

--- model_training_export_and_load.py
+++ model_training_export_and_load.py
@@ -7,6 +7,9 @@ def log_tf_version():
     print(f"TF version: {tf.__version__}")
 
 
+log_tf_version()
+
+
 def create_keras_sequential_model() -> tf.keras.Model:
     return tf.keras.Sequential(
         [
@@ -71,36 +74,51 @@ def create_compile_fit_save_model() -> None:
 
 
 def load_predict_and_assert() -> None:
-    models = {"saved_model_default": None, "keras_tfsm": None, "keras": None}
-
+    all_models_can_be_loaded = True
+    random_input = np.random.random((1, 10)).astype(np.float32)
     for method in ["saved_model_default", "keras_tfsm", "keras"]:
+        predictions = []
         try:
             if method == "saved_model_default":
-                models[method] = load_model(
+                saved_model = load_model(
                     "stored_model_by_saved_model", method="saved_model"
                 )
+                prediction_saved_model = saved_model(random_input).numpy()
+                print(
+                    "Predictions from loaded saved model:",
+                    prediction_saved_model,
+                )
+                predictions.append(prediction_saved_model)
             elif method == "keras_tfsm":
-                models[method] = load_model(
+                tfsm_model = load_model(
                     "stored_model_by_saved_model", method="keras", as_tfsm_layer=True
                 )
+                prediction_keras_model_tfsm = tfsm_model(random_input)[
+                    "dense_1"
+                ].numpy()
+                print(
+                    "Predictions from loaded Keras model (loaded as TFSM) :",
+                    prediction_keras_model_tfsm,
+                )
+                predictions.append(prediction_keras_model_tfsm)
             else:
-                models[method] = load_model("stored_model_by_keras", method="keras")
+                keras_model = load_model(
+                    "stored_model_by_saved_model", method="keras", as_tfsm_layer=False
+                )
+                prediction_keras_model_direct = keras_model.predict(
+                    random_input
+                ).ravel()
+                print(
+                    "Predictions from loaded Keras model (loaded directly):",
+                    prediction_keras_model_direct,
+                )
+                predictions.append(prediction_keras_model_direct)
         except Exception as e:
             print(f"Failed to load {method} model: {e}")
+            all_models_can_be_loaded = False
 
-    if all(models.values()):
-        random_input = np.random.random((1, 10)).astype(np.float32)
-        predictions = {
-            "saved_model": models["saved_model"](random_input),  # pylint: disable=E1102
-            "keras": models["keras"].predict(random_input),
-        }
-        print(
-            "Predictions from loaded saved model:", predictions["saved_model"].numpy()
-        )
-        print("Predictions from loaded Keras model:", predictions["keras"])
-        assert np.allclose(
-            predictions["saved_model"].numpy(), predictions["keras"]
-        ), "Predictions do not match!"
+    if all_models_can_be_loaded:
+        assert np.allclose(np.array(predictions), "Predictions do not match!")
     else:
         print("One of the models could not be loaded, no assertion can be made.")
 
